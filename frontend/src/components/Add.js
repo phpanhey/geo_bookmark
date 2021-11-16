57c77b176af691582646f2806738f3374a0d3422
Philipp Panhey <philipp.panhey@bo-bremen.de>
2021-11-16 17:39:58 +0100

    added add

---
+++ frontend/src/components/Add.js
@@ -0,0 +1,13 @@
+const Add = () => {
+    return (
+        <div className="flex max-auto">
+            <h1 className="font-black text-xl">add your geotag now</h1>
+            <input type="text" placeholder="description"/>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                add
+            </button>
+        </div>
+    )
+}
+
+export default Add
