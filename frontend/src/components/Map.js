1dbce1adf473dc53df0e6d6272a4321f2b94c5d7
Philipp Panhey <philipp.panhey@bo-bremen.de>
2021-11-21 22:12:19 +0100

    added leaflet

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,7 +1,22 @@
+import 'leaflet/dist/leaflet.css';
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 const Map = () => {
     return (
-        <div>
-            <h1>Hallo</h1>   
+        <div className="container">
+            <MapContainer center={[53.095158,8.806068]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[53.095158,8.806068]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
